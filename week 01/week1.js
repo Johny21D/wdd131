88b73c19c1b9abba4fe8368011425872fc618b77
Johny Suy <suy.d.johny15@gmail.com>
2026-01-13 15:27:33 -0700

    changed

---
+++ week 01/week1.js
@@ -0,0 +1,25 @@
+
+const PI = 3.14;
+let radius = 3;
+
+console.log("PI:", PI);
+console.log("Radius:", radius);
+
+radius = 5;
+console.log("Updated Radius:", radius);
+
+
+const one = 1;
+const two = '2';
+
+
+let course = "CSE131"; 
+
+if (true) {
+    let student = "John";
+    console.log("Inside block, course:", course);
+    console.log("Inside block, student:", student);
+}
+
+console.log("Outside block, course:", course);
+console.log("Outside block, student:", student);
